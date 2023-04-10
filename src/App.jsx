a8e0b5075064662b70c1dfb154cd06d0d5c2ee5a
javohir033 <javohirisroilov033@gmail.com>
2023-04-10 15:09:21 +0500

    Asosiy ish tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import brain from './image/memory.svg'
 import reation from './image/vector.svg'
 import visual from './image/visual.svg'
@@ -5,13 +7,50 @@ import chat from '../src/image/reaction.svg';
 import './App.css'
 
 function App() {
+  const [count0, setCount0] = useState(50);
+  const [count1, setCount1] = useState(80);
+  const [count2, setCount2] = useState(92);
+  const [count3, setCount3] = useState(61);
+  const [count4, setCount4] = useState(73);
+  
+
+  function click1() {
+    setCount1(count1 + 1);
+    if (count1 >= 99) {
+      setCount1(100);
+    }
+  }
+  function click2() {
+    setCount2(count2 + 1);
+    if (count2 >= 99) {
+      setCount2(100);
+    }
+  }
+  function click3() {
+    setCount3(count3 + 1);
+    if (count3 >= 99) {
+      setCount3(100);
+    }
+  }
+  function click4() {
+    setCount4(count4 + 1);
+    if (count4 >= 99) {
+      setCount4(100);
+    }
+  }
+  function click0() {
+    setCount0(count0 + 1);
+    if (count0 >= 99) {
+      setCount0(100);
+    }
+  }
   return (
     <div className="container">
       <div className="app">
         <div className="app-left">
           <h2 className='title'>Your Result</h2>
-          <div className='circle'>
-              <h1 className='number'>76</h1>
+          <div onClick={click0} className='circle'>
+              <h1 className='number'>{count0}</h1>
               <span className='full-number'>of 100</span>
           </div>
           <h2 className='status'>Great</h2>
@@ -20,24 +59,44 @@ function App() {
         <div className="app-right">
                   <h2 className="title-r">Summary</h2>
                   <ul className='list'>
-                    <li className='item item1'>
-                      <img className='icon' src={reation} alt="brain" />
-                      <h3 className="item-title color1">Reaction</h3>
+                    <li onClick={click1} className='item item1'>
+                      <div  className="item-left">
+                        <img className='icon' src={reation} alt="brain" />
+                        <h3 className="item-title color1">Reaction</h3>
+                      </div>
+                      <div className='item-right'>
+                        <span className='rating'>{count1}</span>/ 100
+                      </div>
                     </li>
-                    <li className='item item2'>
-                        <img className='icon' src={brain} alt="reation" />
-                        <h3 className="item-title color2">Memory</h3>
+                    <li onClick={click2}  className='item item2'>
+                        <div className="item-left">
+                          <img className='icon' src={brain} alt="reation" />
+                          <h3 className="item-title color2">Memory</h3>
+                        </div>
+                        <div className='item-right'>
+                          <span className='rating'>{count2}</span> / 100
+                        </div>
                     </li>
-                    <li className='item item3'>
-                        <img className='icon' src={chat} alt="visual" />
-                        <h3 className="item-title color3">Verbal</h3>
+                    <li onClick={click3} className='item item3'>
+                        <div className="item-left">
+                          <img className='icon' src={chat} alt="visual" />
+                          <h3 className="item-title color3">Verbal</h3>
+                        </div>
+                        <div className='item-right'>
+                          <span className='rating'>{count3}</span>/ 100
+                        </div>
                     </li>
-                    <li className='item item4'>
-                        <img className='icon' src={visual} alt="chat" />
-                        <h3 className="item-title color4">Visual</h3>
+                    <li onClick={click4} className='item item4'>
+                        <div className="item-left">
+                          <img className='icon' src={visual} alt="chat" />
+                          <h3 className="item-title color4">Visual</h3>
+                        </div>
+                        <div className='item-right'>
+                          <span className='rating'>{count4}</span>/ 100
+                        </div>
                     </li>
                   </ul>
-                  <button className='btn' id='btn'>Continue</button>
+                  <button  className='btn' id='btn'>Continue</button>
               </div>
       </div>
     </div>
@@ -45,14 +104,3 @@ function App() {
 }
 
 export default App;
-
-// const [count, setCount] = useState(0)
-
-{/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
